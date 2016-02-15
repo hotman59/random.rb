39ee537301006d6836d28dd5397fde49e8d6f567
othman <benrahmaneothman@gmail.com>
2016-02-15 16:04:21 +0100

    create  seed dans le fichier seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+a=[
+  "Guillaume",
+  "Sophie",
+  "Jade",
+  "Philippe",
+  "Anas",
+  "Najib",
+  "Marc",
+  "Antoine",
+  "Thierry",
+  "Anthony",
+  "Thomas",
+  "Vincent",
+  "Alexis",
+  "Julie",
+  "Sabrina",
+  "Bertrand",
+  "Naïma",
+  "François",
+  "Othman",
+  "Caroline",
+  "Didier",
+  "Thanh",
+  "Manoël"
+
+]
+23.times do |i|
+  b = Person.create(
+  name: "#{a[i]}",
+  email: "#{a[i]}@exemple.com",
+  tel: 660067190,
+  sensei: false,
+  )
+
+end
