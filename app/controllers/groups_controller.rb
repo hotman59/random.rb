2bf5f51efe2a8c2bbae99ee59c17b095b184332e
othman <benrahmaneothman@gmail.com>
2016-02-16 10:32:34 +0100

    add listing  name user in group index (page principal)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
   # GET /groups.json
   def index
     @groups = Group.all
+    @people = Person.all
   end
 
   # GET /groups/1
@@ -70,5 +71,6 @@ class GroupsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
       params.require(:group).permit(:name_group, :salle, :tache)
+      params.require(:person).permit(:name, :email, :tel, :group_id, :sensei)
     end
 end
